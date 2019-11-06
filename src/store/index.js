c2b7409c6ede8f2d66df92486843313da992d998
shifawei <451154708@qq.com>
2019-11-06 17:09:51 +0800

    update index.js
    命名修改

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,11 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    list: []
+    lists: []
   },
   mutations: {
     addItem (state, value) {
-      state.list.push(value)
+      state.lists.push(value)
     }
   },
   actions: {
